a4fffadd4195ab6848b74e887f738ba0f8d3c40b
nikhilmohan97 <NGundavaram@dal.ca>
2019-05-23 10:03:45 -0230

    printing multiples 3 and 5 has fizz and buzz

--- task0.py
+++ task0.py
@@ -1,10 +1,11 @@
 def fizzbuzz(max_num=101):
 	for i in range(max_num):
 	    print(i)
+            value = ""
+            if i % 3 == 0: value +="fizz"
+	    if i % 5 == 0: value +="buzz"
+            yield value if value else i
 
 
-
-
-
-
-fizzbuzz()
+for number, burp in enumerate(fizzbuzz()):
+    print "%s: %s" % (number, burp)
